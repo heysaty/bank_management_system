11387a0cc66ee05a60520ebddcac49751e4c7e4c
heysaty <satyam@gkmit.co>
2022-10-18 14:15:15 +0530

    encrypted the password and authenticated the user

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+
+
 import config
+
+
 
 app = Flask(__name__)
 
@@ -8,9 +13,11 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = config.db_connection()
-app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_DB'] = 'bank_data'
 
 mysql = MySQL(app)
+
+bcrypt = Bcrypt()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,19 +29,51 @@ def index():
         last_name = userDetails['last_name']
         email = userDetails['email']
         phone_no = userDetails['phone_no']
-        password = userDetails['password']
+        password=userDetails['password']
+        password = bcrypt.generate_password_hash(password)
+
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(first_name,last_name,email,phone_no,password) VALUES(%s,%s,%s,%s,%s)",
-                    (first_name, last_name, email, phone_no, password))
+
+        cur.execute("SELECT * FROM users WHERE email LIKE %s", [email])
+
+        rows = cur.fetchone()
+        if rows:
+            return 'email already exists'
+        else:
+            cur.execute("INSERT INTO users(first_name,last_name,email,phone_no,password) VALUES(%s,%s,%s,%s,%s)",(first_name, last_name, email, phone_no, password))
         mysql.connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
 
+
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # store the form data
+        userDetails = request.form
+        email = userDetails['email']
+        password = userDetails['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email LIKE %s", [email])
+
+
+        rows = cur.fetchone()
+        if rows:
+            auth=bcrypt.check_password_hash( rows[-1],password)
+            if auth:
+                return render_template('homepage.html')
+        else:
+            return "wrong credentials"
     return render_template('login.html')
+
+
+
+
 
 @app.route('/homepage', methods=['GET', 'POST'])
 def homepage():
